81e7de4190294e0ac57f1ec978ddad7c55050bea
Matheus Raposo <raposomatheus@outlook.com.br>
2023-07-16 10:42:33 -0300

    criado funcao moeda e chamando ela dentro da main.

--- ConversorDeMoedas/src/moedas.java
+++ ConversorDeMoedas/src/moedas.java
@@ -2,13 +2,16 @@ import javax.swing.JOptionPane;
 
 public class moedas {
 	public static void main(String[] args) {
+		moeda();
+	}
+	public static void moeda() {
 		String real = JOptionPane.showInputDialog("Valor em real: ");
-		String moeda = JOptionPane.showInputDialog("Valor da cotacão: ");
+		String moeda = JOptionPane.showInputDialog("Valor da cotacão da moeda que deseja: ");
 		
 		Double dReal = Double.parseDouble(real);
 		Double dMoeda = Double.parseDouble(moeda);
 		
-		double conversao = dReal * dMoeda;
+		double conversao = dReal / dMoeda;
 		
 		JOptionPane.showMessageDialog(null, "O valor da conversão atual é: " + conversao +  " Reais");
 	}
